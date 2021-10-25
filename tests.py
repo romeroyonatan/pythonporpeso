89417ea157eb1171b1f2a71e87bb8ce9fd611b4d
Yonatan Romero <romeroyonatan@gmail.com>
2021-10-25 20:36:06 +0200

    primer test pasa

--- tests.py
+++ tests.py
@@ -1,2 +1,14 @@
+class Peso:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        if self.value == 1:
+            return "1 peso"
+        else:
+            return f"{self.value} pesos"
+
+
 def test1():
-    assert Peso(1) == "1 peso"
+    assert str(Peso(1)) == "1 peso"
+    assert str(Peso(2)) == "2 pesos"
